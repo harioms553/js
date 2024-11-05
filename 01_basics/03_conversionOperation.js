3f6922e2110de5ab2c5599eab269c8eefb3066a3
Hariom Singh <hariomsing553@gmail.com>
2024-11-05 11:14:01 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); //NaN not a number
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); //NaN not a number
 
 // "33" ==> 33
 // "33abc" => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber); //NaN not a number
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); 
+//console.log(booleanIsLoggedIn); 
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,5 +24,33 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
  
 let stringNumber = String(someNumber)
-console.log(stringNumber); 
-console.log(typeof stringNumber); 
+//console.log(stringNumber); 
+//console.log(typeof stringNumber); 
+
+//*****************************************************Operations**************************
+
+let value = 3
+let negValue = -value
+//console.log();
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 =" hariom"
+let str3 = str1 + str2
+//console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //if string is at begining, then it converts everything remaining as string
+// console.log(1 + 2 + "2");
+
+// console.log(+true)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++ //or ++gameCounter
+
+//link to study 
+// 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//avoid above comaprision and have clean ccode
+
+//=== strict check
+
+console.log("2" === 2);
