d6233c898327fcea96dbbcad8c4228499c0c8a68
Hariom Singh <hariomsing553@gmail.com>
2024-11-05 06:22:34 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "hariomsing553@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+/*
+Prefer not to use var
+bcz of issue in block scope and functional scope
+*/
+
+
+accountEmail ="dkjhdkh.com"
+accountPassword = "2370340"
+accountCity = "Bengaluru"
+
+console.log(accountEmail)
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log (3+
+    3) // code readability should be high
+
+let name ="hariom"
+let age = 35
+let isLoggedIn = false
+let state = null
+
+// number => 2 to the power 53
+// bigint == big number
+// string => ""
+// bolean ==> true/false
+// null => standalone value
+// undefined => no value
+// symbol => unique value then we use symbol
+
+// object
+
+console.log(typeof "hitesh")
+console.log(typeof null)       // object
+console.log(typeof undefined)  // undefined
